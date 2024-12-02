3ff65a9f6bad6e7edc8821146376511d4fcff994
MUKILTEO\1717479 <drakondlss+Github@gmail.com>
2024-12-02 08:49:02 -0800

    Fixed player movement

--- Mutant Factory/Assets/Scripts/Movement.cs
+++ Mutant Factory/Assets/Scripts/Movement.cs
@@ -6,16 +6,12 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private float move;
-    bool grounded;
 
     private Vector2 CheckyPointy;
     
     public KeyCode left, right, up, down;
     public float maxSpeed;
     public float buildUp;
-
-    public KeyCode jump;
-    public float jumpForce;
 
     private Rigidbody2D moveit;
     void Start()
@@ -39,28 +35,6 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(down))
         {
             moveit.AddForce(Vector2.down * buildUp);
-        }
-
-        if (Input.GetKeyDown(jump) && grounded == true)
-        {
-            moveit.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-
-        moveit.velocity=new Vector2(Mathf.Clamp(moveit.velocity.x, -maxSpeed, maxSpeed), moveit.velocity.y);
-    }
-    private void OnCollisionStay2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            grounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-         if (other.gameObject.CompareTag("Ground"))
-        {
-            grounded = false;
         }
     }
 }
