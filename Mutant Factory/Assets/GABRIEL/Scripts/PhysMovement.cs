548b2b394d51c54e2dc06f14081f632c7bbbb7da
Gabriel Tsircou <1722710@mukilteo.wednet.edu>
2024-12-17 10:12:46 -0800

    finished mc animations, also tweaked movement script

--- Mutant Factory/Assets/GABRIEL/Scripts/PhysMovement.cs
+++ Mutant Factory/Assets/GABRIEL/Scripts/PhysMovement.cs
@@ -8,21 +8,27 @@ public class PhysMovement : MonoBehaviour
 {
     //keycodes!!!!!!!!!!!!!!!!
     public KeyCode left = KeyCode.A, right = KeyCode.D, up = KeyCode.W, down = KeyCode.S;
+
     // sets move speed
     public float speed = 10;
+
     // vectors for animators
-    //public bool fWalk = false;
-    //public bool uWalk = false;
-    //public bool lWalk = false;
-    //public bool rWalk = false;
+    public bool fWalk = false;
+    public bool uWalk = false;
+    public bool lWalk = false;
+    public bool rWalk = false;
     public bool walk = false;
+
     // bool for toggling inertia
     public bool hasNoMass = false;
-    public Animator animator;
+
+    // bool for toggling asset flipping
+    public bool flipAsset = true;
+
     private Rigidbody2D _rb2d;
 
+    public Animator animator;
     public SpriteRenderer SpriteRenderer;
-    public float input;
 
     void Start() //Start is called before the first frame update
     {
@@ -31,67 +37,80 @@ public class PhysMovement : MonoBehaviour
 
     void Update()  // Update is called once per frame
     {
-        //animator.SetBool("walk forward", fWalk);
-        //animator.SetBool("walk up", uWalk);
-        //animator.SetBool("walk left", lWalk);
-        //animator.SetBool("walk right", rWalk);
-        animator.SetBool("iswalk", walk);
-        // sets the velocity to zero every frame if true
-        if (hasNoMass == true)
+        animator.SetBool("walk", walk);
+        animator.SetBool("walk forward", fWalk);
+        animator.SetBool("walk up", uWalk);
+        animator.SetBool("walk left", lWalk);
+        animator.SetBool("walk right", rWalk);
+        
+        if (hasNoMass == true) // sets the velocity to zero every frame if true
         {
-           _rb2d.velocity = Vector2.zero;
+            _rb2d.velocity = Vector2.zero;
         }
 
+        // toggles each bool and moves character when key pressed, resets when not moving
         if (Input.GetKey(left))
         {
-           { 
+            walk = true;
+            lWalk = true;
+            rWalk = false;
+            uWalk = false;
+            fWalk = false;
            _rb2d.velocity = Vector2.left * speed;
-           //lWalk = true;
-           SpriteRenderer.flipX = true;
-           walk = true;
-           }
+            if (flipAsset == true)
+            {
+                SpriteRenderer.flipX = true;
+            }
         }
-        else
+        else if (Input.GetKey(right))
         {
-            //lWalk = false;
-            walk = false;
-        }
-
-        if (Input.GetKey(right))
-        {
+            walk = true;
+            rWalk = true;
+            lWalk = false;
+            uWalk = false;
+            fWalk = false;
+            if (flipAsset == true)
+            {
+                SpriteRenderer.flipX = false;
+            }
             _rb2d.velocity = Vector2.right * speed;
-           //rWalk = true;
-           SpriteRenderer.flipX = false;
-           walk = true;
         }
-        else
+        else if (Input.GetKey(up))
         {
-            //rWalk = false;
-            walk = false;
-        }
-
-        if (Input.GetKey(up))
-        {
+            walk = true;
+            uWalk = true;
+            lWalk = false;
+            rWalk = false;
+            fWalk = false;
             _rb2d.velocity = Vector2.up * speed;
-            //uWalk = true;
-            walk = true;
-        }
-        else
-        {
-            //uWalk = false;
-            walk = false;
-        }
 
-        if (Input.GetKey(down))
+        }
+        else if (Input.GetKey(down))
         {
-            _rb2d.velocity = Vector2.down * speed;
-            //fWalk = true;
             walk = true;
+            fWalk = true;
+            uWalk = false;
+            rWalk = false;
+            lWalk = false;
+            _rb2d.velocity = Vector2.down * speed;
         }
         else
         {
-            //fWalk = false;
             walk = false;
+            fWalk = false;
+            uWalk = false;
+            rWalk = false;
+            lWalk = false;
         }
     }
 }
+
+// THIS PROGRAM IS PROPERTY OF
+//      ________   ___    __   _______   _____   ___    __   ________
+//     //      /   //|    /   //          //     //|    /   //       
+//    //======/   // |   /   //_____     //     // |   /   //__      
+//   //      /   //  |  /          /    //     //  |  /   //         
+//  //      /   //   | /          /    //     //   | /   //          
+// //      /  _//_  _|/_   ______/  __//__  _//_  _|/_  //______ TM
+//
+// ANSINE CORP.TM COPYRIGHT 1998 - 2024
