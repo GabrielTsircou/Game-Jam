aea6559517e2c577b077372b2965651cb05c3a55
Derek <dereksturm263@gmail.com>
2025-01-06 08:52:18 -0800

    inventory stuff

---
+++ Mutant Factory/Assets/Derek/Scripts/Flashlight.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Flashlight : Item
+{
+    public override void OnEquip(GameObject player)
+    {
+
+    }
+
+    public override void OnUnequip(GameObject player)
+    {
+
+    }
+
+    public override void OnUse(GameObject player)
+    {
+
+    }
+}
